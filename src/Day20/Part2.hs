cfce7140b1f97b4d526cebf64fdb9a3d752f7d7a
Joa <joa.oris@hotmail.com>
2020-12-20 12:32:07 +0100

    Day 20

--- src/Day20/Part2.hs
+++ src/Day20/Part2.hs
@@ -10,6 +10,7 @@ import qualified Data.Map as M
 import Data.List
 import Data.Maybe
 import Debug.Trace
+import qualified Data.Vector as V
 
 type Grid = [[Bool]]
 data Tile = Tile Int Grid
@@ -41,13 +42,20 @@ addBorders tile@(Tile id grid) = TWB tile $ map bitsToInt borders
 
 type Position = (Int, Int)
 
-seaMonster :: [Position]
-seaMonster = [(x, y) | x <- [0 .. length (head image) - 1], y <- [0 .. length image - 1], image !! y !! x == '#'] where
+seaMonster :: Orientation -> [Position]
+seaMonster dir = [transformCoord dir (x, y) | x <- [0 .. w - 1], y <- [0 .. h - 1], image !! y !! x == '#'] where
     image = [
             "                  # ",
             "#    ##    ##    ###",
             " #  #  #  #  #  #   "
         ]
+    w = length $ head image
+    h = length image
+    transformCoord (d, FLIPPED) (x, y) = transformCoord (d, UNFLIPPED) (w-x-1, y)
+    transformCoord (UP, _) (x, y) = (x, y)
+    transformCoord (RIGHT, _) (x, y) = (h-y-1, x)
+    transformCoord (DOWN, _) (x, y) = (w-x-1, h-y-1)
+    transformCoord (LEFT, _) (x, y) = (y, w-x-1)
 
 data Dir = UP | RIGHT | DOWN | LEFT deriving (Show, Eq, Enum)
 data Flip = UNFLIPPED | FLIPPED deriving (Show, Eq, Enum)
@@ -66,18 +74,6 @@ oppF FLIPPED = UNFLIPPED
 
 indexD d = 2 * fromEnum d
 indexF f = fromEnum f
-
--- getBorder :: Orientation -> [a] -> a
--- getBorder (d, f) = (!! (getIndex d + if f == FLIPPED then 1 else 0))
-
--- getBorder :: Orientation -> Orientation -> [a] -> a
--- getBorder (d1, f1) (d2, f2) = (!! ((getIndex (applyFlip f1 d1) + getIndex (applyFlip f2 d2)) `mod` 8)) . cycle where
---     applyFlip _ UP = UP
---     applyFlip _ DOWN = DOWN
---     applyFlip UNFLIPPED LEFT = LEFT
---     applyFlip UNFLIPPED RIGHT = RIGHT
---     applyFlip FLIPPED LEFT = RIGHT
---     applyFlip FLIPPED RIGHT = LEFT
 
 flip' :: Orientation -> Orientation
 flip' (d, f) = (if d `elem` [UP, DOWN] then d else oppD d, oppF f)
@@ -98,6 +94,39 @@ tilesInDirection borderMap tile@(TWB (Tile id _) bs, or) dir = if length (border
     ttr@(TWB _ rbs) = head $ filter (\(TWB (Tile id' _) _) -> id' /= id) $ borderMap M.! border
     ttrOr = head [ttrOr | ttrOr <- allOrientations, getBorder (oppD dir, FLIPPED) ttrOr rbs == border]
 
+gridValueAt :: Position -> Tile -> Bool 
+gridValueAt (x, y) (Tile _ grid) | x >= 0 && y >= 0 && x < 8 && y < 8 = grid !! (y+1) !! (x+1)
+
+transformCoord :: Orientation -> Position -> Position
+transformCoord (d, FLIPPED) (x, y) = transformCoord (d, UNFLIPPED) (7-x, y)
+transformCoord (UP, _) (x, y) = (x, y)
+transformCoord (RIGHT, _) (x, y) = (7-y, x)
+transformCoord (DOWN, _) (x, y) = (7-x, 7-y)
+transformCoord (LEFT, _) (x, y) = (y, 7-x)
+
+imageValueAt :: Position -> [[PlacedTile]] -> Bool 
+imageValueAt (x, y) tiles = gridValueAt (transformCoord or (x', y')) tile where
+    tx = x `div` 8
+    ty = y `div` 8
+    x' = x `mod` 8
+    y' = y `mod` 8
+    (TWB tile _, or) = tiles !! ty !! tx
+
+possibleOffsets :: [Position] -> [Position]
+possibleOffsets positions = [(x, y) | x <- [negate minX .. 12 * 8 - maxX - 1], y <- [negate minY .. 12 * 8 - maxY - 1]] where
+    minX = minimum $ map fst positions
+    minY = minimum $ map snd positions
+    maxX = maximum $ map fst positions
+    maxY = maximum $ map snd positions
+
+match :: V.Vector Bool -> Position -> Bool
+match image (x, y) = image V.! (x + 12*8*y)
+
+matches :: V.Vector Bool -> Position -> [Position] -> Bool
+matches image offset = all (match image . add offset)
+
+add (x, y) (x', y') = (x+x', y+y')
+
 solution = do
     tiles <- map addBorders <$> readParsed (Day 20) (many parseTile)
     let allBorders = concatMap (\(TWB _ b) -> b) tiles
@@ -107,5 +136,11 @@ solution = do
     let borderMap = M.fromListWith (<>) $ concatMap (\t@(TWB _ bs) -> map (,[t]) bs) tiles
     let leftTiles = tilesInDirection borderMap (topLeft, tlOr) DOWN
     let tileGrid = map (\t -> tilesInDirection borderMap t RIGHT) leftTiles
-    print $ length tileGrid
-    print $ map length tileGrid
+    let image = V.fromList [imageValueAt (i `mod` (12*8), i `div` (12*8)) tileGrid | i <- [0 .. 12 * 12 * 8 * 8 - 1]]
+    let foundSeamonsters = [[(offs, map (add offs) (seaMonster or)) | offs <- possibleOffsets (seaMonster or), matches image offs (seaMonster or)] | or <- allOrientations]
+    print foundSeamonsters
+    let allPosses = concatMap snd $ foundSeamonsters !! 7
+    unless (null [p | p <- allPosses, length (filter (==p) allPosses) /= 1]) $ putStrLn "Duplicates found"
+    let nbMonsters = maximum $ map length foundSeamonsters
+    print nbMonsters
+    print $ length (filter id $ V.toList image) - length allPosses
