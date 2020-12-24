55461aea923be0be034ab45709806bebae6d5aaa
Joa <joa.oris@hotmail.com>
2020-12-24 08:50:03 +0100

    Day 23 Cleanup

--- src/Day23/Part2.hs
+++ src/Day23/Part2.hs
@@ -1,11 +1,6 @@
 module Day23.Part2 where
 
-import Data.List
-import Data.Maybe
 import qualified Data.Sequence as S
-import Data.Sequence (Seq((:<|), (:|>)))
-import Data.Foldable (Foldable(toList))
-import Debug.Trace
 
 type Cup = Int
 type Cups = S.Seq Int
@@ -16,16 +11,15 @@ puzzleInput = map (read . (:[])) "586439172" <> [10..1000000]
 createCups :: [Cup] -> Cups
 createCups input = foldr (\(i, v) -> S.adjust' (const v) i) (S.replicate (length input + 1) 0) [(a, b) | (a, b) <- zip input (drop 1 input <> [head input])]
 
-s ! i = fromJust $ s S.!? i
+(!) :: S.Seq a -> Int -> a
+(!) = S.index
 
 playRound :: (Cups, Cup) -> (Cups, Cup)
-playRound (cups, currentCup) = (cups''', cups''' ! currentCup) where
+playRound (cups, currentCup) = (cups', cups' ! currentCup) where
     next = (cups !)
     cupsToMove = take 3 $ drop 1 $ iterate next currentCup
     destinationCup = head [c | c <- [currentCup-1, currentCup-2, currentCup-3, currentCup-4, 1000000, 999999, 999998, 999997], c > 0, c `notElem` cupsToMove]
-    cups' = S.update currentCup (next $ last cupsToMove) cups
-    cups'' = S.update (last cupsToMove) (next destinationCup) cups'
-    cups''' = S.update destinationCup (head cupsToMove) cups''
+    cups' = foldr (uncurry S.update) cups [(currentCup, next $ last cupsToMove), (last cupsToMove, next destinationCup), (destinationCup, head cupsToMove)]
 
 solution = do
     let solution = fst $ (!! 10000000) $ iterate playRound (createCups puzzleInput, head puzzleInput)
